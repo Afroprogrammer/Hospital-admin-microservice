78b8233c92ad7f1f879a7f48dc0fdfa79d63274b
Franklin <forsonfranklin@gmail.com>
2021-08-05 08:07:24 +0000

    created the endpoints to get a list of patients and one patient by id

--- admissions-service/src/main/java/com/afroprogrammer/admissionsservice/controller/AdmissionsController.java
+++ admissions-service/src/main/java/com/afroprogrammer/admissionsservice/controller/AdmissionsController.java
@@ -1,7 +1,29 @@
 package com.afroprogrammer.admissionsservice.controller;
 
+import com.afroprogrammer.admissionsservice.model.Patient;
+import com.afroprogrammer.admissionsservice.service.AdmissionsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
+@RequestMapping("/admissions")
 public class AdmissionsController {
+
+@Autowired
+    AdmissionsService admissionsService;
+    @GetMapping("/patients")
+    public List<Patient> getPatients(){
+        return  admissionsService.getPatients();
+    }
+
+    @GetMapping("/patients/{Id}")
+    public Patient getPatientById(@PathVariable("Id") String Id){
+       return admissionsService.getPatientById("Id");
+    }
 }

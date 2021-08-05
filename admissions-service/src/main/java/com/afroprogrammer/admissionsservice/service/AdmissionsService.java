fe9eb9beeb02476b4264d9a5ad2c5ce1003a3829
Franklin <forsonfranklin@gmail.com>
2021-08-05 08:09:33 +0000

    created a service that would return a list and one Patient by id

--- admissions-service/src/main/java/com/afroprogrammer/admissionsservice/service/AdmissionsService.java
+++ admissions-service/src/main/java/com/afroprogrammer/admissionsservice/service/AdmissionsService.java
@@ -1,7 +1,32 @@
 package com.afroprogrammer.admissionsservice.service;
 
+import com.afroprogrammer.admissionsservice.model.Patient;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class AdmissionsService {
+    List<Patient> patients = Arrays.asList(
+            new Patient("P1", "Gabor", "Humgarian"),
+            new Patient("P2", "Joe", "Ghana"),
+            new Patient("P3", "Franklin", "Mali"),
+            new Patient("P4", "Ben", "Russia")
+    );
+
+    public List<Patient> getPatients(){
+        return  patients;
+    }
+
+    public Patient getPatientById(String Id)
+    {
+        Patient p = patients.stream()
+                .filter(patient -> Id.equals(patient.getId()))
+                .findAny()
+                .orElse(null);
+        return p;
+    }
+
 }

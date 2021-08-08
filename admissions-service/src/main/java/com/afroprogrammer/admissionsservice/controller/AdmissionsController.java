1e3d63eaa9d3489e886e722fa125b409d3fb6ef8
Franklin <forsonfranklin@gmail.com>
2021-08-08 01:10:00 +0000

    updated admission service files

--- admissions-service/src/main/java/com/afroprogrammer/admissionsservice/controller/AdmissionsController.java
+++ admissions-service/src/main/java/com/afroprogrammer/admissionsservice/controller/AdmissionsController.java
@@ -1,5 +1,7 @@
 package com.afroprogrammer.admissionsservice.controller;
 
+import com.afroprogrammer.admissionsservice.model.DiseaseList;
+import com.afroprogrammer.admissionsservice.model.EmployeesList;
 import com.afroprogrammer.admissionsservice.model.Patient;
 import com.afroprogrammer.admissionsservice.service.AdmissionsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,22 +10,34 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/admissions")
 public class AdmissionsController {
 
-@Autowired
+
+    @Autowired
     AdmissionsService admissionsService;
+
     @GetMapping("/patients")
-    public List<Patient> getPatients(){
-        return  admissionsService.getPatients();
+    public List<Patient> getPatients() {
+        return admissionsService.getPatients();
     }
 
-    @GetMapping("/patients/{Id}")
-    public Patient getPatientById(@PathVariable("Id") String Id){
-       return admissionsService.getPatientById("Id");
+    @GetMapping("/patient/{Id}")
+    public Patient getPatientById(@PathVariable("Id") String Id) {
+        return admissionsService.getPatientById(Id);
+    }
+
+    @GetMapping("/physicians")
+    public EmployeesList getEmployees() {
+        return admissionsService.getEmployees();
+    }
+
+    @GetMapping("/diseases")
+    public DiseaseList getDisease() {
+        return admissionsService.getDiseaseList();
     }
 }
--- admissions-service/src/main/java/com/afroprogrammer/admissionsservice/service/AdmissionsService.java
+++ admissions-service/src/main/java/com/afroprogrammer/admissionsservice/service/AdmissionsService.java
@@ -1,32 +1,47 @@
 package com.afroprogrammer.admissionsservice.service;
 
+import com.afroprogrammer.admissionsservice.model.DiseaseList;
+import com.afroprogrammer.admissionsservice.model.EmployeesList;
 import com.afroprogrammer.admissionsservice.model.Patient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AdmissionsService {
+    @Autowired
+    private RestTemplate restTemplate;
+
     List<Patient> patients = Arrays.asList(
-            new Patient("P1", "Gabor", "Humgarian"),
+            new Patient("P1", "Gabor", "Hugarian"),
             new Patient("P2", "Joe", "Ghana"),
-            new Patient("P3", "Franklin", "Mali"),
-            new Patient("P4", "Ben", "Russia")
+            new Patient("P3", "Franklin", "Mali")
     );
 
-    public List<Patient> getPatients(){
-        return  patients;
+    public List<Patient> getPatients() {
+        return patients;
     }
 
-    public Patient getPatientById(String Id)
-    {
-        Patient p = patients.stream()
-                .filter(patient -> Id.equals(patient.getId()))
+    public Patient getPatientById(String Id) {
+        Patient patient = patients.stream()
+                .filter(x -> Id.equals(x.getId()))
                 .findAny()
                 .orElse(null);
-        return p;
+        return patient;
     }
 
+    public EmployeesList getEmployees() {
+        EmployeesList employeeList = restTemplate.getForObject("http://hr-service/hr/employees", EmployeesList.class);
+        return employeeList;
+    }
+
+    public DiseaseList getDiseaseList() {
+        DiseaseList diseaseList = restTemplate.getForObject("http://pathology-service/pathology/diseases", DiseaseList.class);
+        return diseaseList;
+    }
 }
